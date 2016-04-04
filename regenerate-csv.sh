e0e76c19e9a7605f45c56fbfdf732668504d5af4
Conal Tuohy <conal.tuohy@gmail.com>
2016-04-04 17:52:19 +1000

    update bash script to run SPARQL queries and generate CSV

--- regenerate-csv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# The "--fail" option is used to specify that server error reports should be swallowed rather than over-writing an existing CSV file
-# Headers specify that the content is SPARQL and that the desired output is CSV.
-# The "--data-binary" option specifies that the SPARQL file should be sent as is, without stripping end of line markers as "--data" would.
-# The "-X POST" option specifies an HTTP POST.
-curl -X POST --fail --header "Accept: text/csv" --header "Content-Type: application/sparql-query" --data-binary "@sparql/nodes.rq" --output "nodes.csv" http://prov-data.conaltuohy.com/fuseki/prov/query
-curl -X POST --fail --header "Accept: text/csv" --header "Content-Type: application/sparql-query" --data-binary "@sparql/function-agency-relationships.rq" --output "agency-functions.csv" http://prov-data.conaltuohy.com/fuseki/prov/query
---
+++ sparql/regenerate-csv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# The "--fail" option is used to specify that server error reports should be swallowed rather than over-writing an existing CSV file
+# Headers specify that the content is SPARQL and that the desired output is CSV.
+# The "--data-binary" option specifies that the SPARQL file should be sent as is, without stripping end of line markers as "--data" would.
+# The "-X POST" option specifies an HTTP POST.
+curl -X POST --fail --header "Accept: text/csv" --header "Content-Type: application/sparql-query" --data-binary "@nodes.rq" --output "/var/www/html/provisualizer/data/nodes.csv" http://localhost:8080/fuseki/prov/query
+curl -X POST --fail --header "Accept: text/csv" --header "Content-Type: application/sparql-query" --data-binary "@function-agency-relationships.rq" --output "/var/www/html/provisualizer/data/agency-functions.csv" http://localhost:8080/fuseki/prov/query
