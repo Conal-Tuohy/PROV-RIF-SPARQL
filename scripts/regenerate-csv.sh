429709ec1afdad578eee1d07cd17f010d41fa467
Conal Tuohy <conal.tuohy@gmail.com>
2016-07-19 04:45:27 +0000

    set directory to where the SPARQL queries are located

--- scripts/regenerate-csv.sh
+++ scripts/regenerate-csv.sh
@@ -3,5 +3,6 @@
 # Headers specify that the content is SPARQL and that the desired output is CSV.
 # The "--data-binary" option specifies that the SPARQL file should be sent as is, without stripping end of line markers as "--data" would.
 # The "-X POST" option specifies an HTTP POST.
+cd /opt/provisualizer/scripts
 curl -X POST --fail --header "Accept: text/csv" --header "Content-Type: application/sparql-query" --data-binary "@nodes.rq" --output "/var/www/html/provisualizer/data/nodes.csv" http://localhost:8080/fuseki/prov/query
 curl -X POST --fail --header "Accept: text/csv" --header "Content-Type: application/sparql-query" --data-binary "@edges.rq" --output "/var/www/html/provisualizer/data/edges.csv" http://localhost:8080/fuseki/prov/query
